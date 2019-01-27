f7e4c93e32a4b4f1523bb7e6c140bf1b83f56d2b
lilinailparis <39217046+lilinailparis@users.noreply.github.com>
2019-01-27 17:34:25 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,7 +7,23 @@ $(function(){
     $('html, body').animate({scrollTop: dn}, 500);
   });
 });
-/* slider */
-var elem = document.getElementById('mySwipe');
-window.mySwipe = Swipe(elem, {
+
+/* on off menu */
+$('.on_off').on('click', function(e) {
+	e.preventDefault();
+ $('.submenu').toggleClass('menu_on');
+});
+
+/* language menu */
+$('.menu-btn').on('click', function(e) {
+  e.preventDefault();
+  $(this).toggleClass('menu-btn_active');
+  $('.menu_lang').toggleClass('menu_active');
+});
+
+$('.ru').on('click', function() {
+  alert ('Учи английский язык !')
+});
+$('.fr').on('click', function() {
+  alert ('Учи английский язык !')
 });
